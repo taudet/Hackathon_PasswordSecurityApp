e7b7f3afd2b6621be0dec28326184a3093461211
taudet <thomas.audet@live.com>
2025-01-25 16:30:52 -0500

    added checks for int and letter count to add to complexity

--- PassWordStrengthTester/PassWordStrengthTester/Program.cs
+++ PassWordStrengthTester/PassWordStrengthTester/Program.cs
@@ -21,12 +21,22 @@ while ((line = reader.ReadLine()) != null)
 
 
 int specialCharCount = 0;
+int intCount = 0;
+int alphaCount = 0;
 
 for (int i = 0; i < incomingPassword.Length; i++)
 {
     if (!char.IsLetterOrDigit(incomingPassword[i]))
     {
         specialCharCount++;
+    }
+    else if (char.IsDigit(incomingPassword[i]))
+    {
+        intCount++;
+    }
+    else
+    {
+        alphaCount++;
     }
 }
 
@@ -44,11 +54,47 @@ else
     passwordScore++;
 }
 
+if (intCount == 0)
+{
+    passwordScore -= 2;
+}
+else if (intCount > 0 && intCount <= 6)
+{
+    passwordScore --;
+}
+else
+{
+    passwordScore += 1;
+}
+
+if (alphaCount == 0)
+{
+    passwordScore -= 2;
+}
+else if (alphaCount > 0 && intCount <= 6)
+{
+    passwordScore--;
+}
+else
+{
+    passwordScore += 1;
+}
+
 if (incomingPassword.Length <= 5)
 {
-    passwordScore = passwordScore - 2;
+    passwordScore -= 2;
 }
-else if( incomingPassword.Length <= 10 && incomingPassword.Length >= 6)
+else if(incomingPassword.Length <= 10 && incomingPassword.Length >= 6)
 {
-
+    passwordScore --;
 }
+else if (incomingPassword.Length <= 11 && incomingPassword.Length >= 16)
+{
+    passwordScore++;
+}
+else
+{
+    passwordScore += 2;
+}
+
+Console.WriteLine(passwordScore);
