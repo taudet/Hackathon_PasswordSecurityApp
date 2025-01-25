95fa70e124fb62df583b62b5bb320ecc14fc176c
taudet <thomas.audet@live.com>
2025-01-25 15:03:19 -0500

    added a basic score to keep track of the password strength, compare password against common password list to check strenght and check for special characters

--- PassWordStrengthTester/PassWordStrengthTester/Program.cs
+++ PassWordStrengthTester/PassWordStrengthTester/Program.cs
@@ -1,20 +1,54 @@
-﻿
+﻿string incomingPassword = args[0];
 
-string incomingPassword = args[0];
-
-string passwordList = "C:ProjectFiles\\10-million-password-list-top-10000.txt";
+string passwordList = "10-million-password-list-top-1000000.txt";
 
 StreamReader reader = new StreamReader(passwordList);
 
 bool passwordFound = false;
+int passwordScore = 0;
 
-while (true)
+string line;
+while ((line = reader.ReadLine()) != null)
 {
-    string line = reader.ReadLine();
 
     if (line == incomingPassword)
     {
         passwordFound = true;
+        passwordScore = passwordScore -100;
         break;
     }
+}
+
+
+int specialCharCount = 0;
+
+for (int i = 0; i < incomingPassword.Length; i++)
+{
+    if (!char.IsLetterOrDigit(incomingPassword[i]))
+    {
+        specialCharCount++;
+    }
+}
+
+
+if (specialCharCount == 0)
+{
+    passwordScore = passwordScore - 2;
+}
+else if (specialCharCount == 1)
+{
+    passwordScore--;
+}
+else
+{
+    passwordScore++;
+}
+
+if (incomingPassword.Length <= 5)
+{
+    passwordScore = passwordScore - 2;
+}
+else if( incomingPassword.Length <= 10 && incomingPassword.Length >= 6)
+{
+
 }
